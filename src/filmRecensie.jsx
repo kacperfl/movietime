ccd9272c325aca3bca26d7b94647bbf617d84747
kacperfl <kacperfl29@gmail.com>
2024-10-03 18:23:38 +0200

    optimalisatie aan de pagina

--- src/filmRecensie.jsx
+++ src/filmRecensie.jsx
@@ -1,84 +1,92 @@
-// er worden paar dingen geimporteerd om project te runnen zoals standard react en de state useref en useeffect wat worden gebruikt voor 
-//aparte basis functies voor deze pagina, daarnaast ook icon library
 import React, { useState, useRef, useEffect } from 'react';
 import './styling/filmrecensie.scss';
-import { FaStar } from 'react-icons/fa'; 
-// aantal arrays die waarden van 0 - null hebben
+import { FaStar } from 'react-icons/fa';
+
 const Recensie = () => {
   const [selectedFilm, setSelectedFilm] = useState(null);
   const [recensie, setRecensie] = useState('');
   const [beoordeling, setBeoordeling] = useState(0);
   const [recensies, setRecensies] = useState([]);
+  const [films, setFilms] = useState([]);
   const reviewRef = useRef(null);
 
-
-  //dit is het lijstje van handmatige toegevoegde films ze spelen nu voornameklijk rol als een placeholders
-  const films = [
+  //3. de fotos zijn gecompresseerd waardoor ze minder qua grootte hebben en sneller inladen
+  // 3. de foto formaat is geoptimaliseerd
+  const defaultFilms = [
     { id: 1, foto: 'src/photos/ATLAS-KEYART.jpg', title: 'Atlas' },
-    { id: 2, foto: 'src/photos/6f690089947a98659643e2ce8f5f0200-647x1024.png', title: 'Godzilla X KONG The New Empire' },
+    // { id: 2, foto: 'src/photos/JokerFolieC3A0Deux_133650099276711151_big.jpg', title: 'JokerFolie' },
     { id: 3, foto: 'src/photos/398055860_688837486526142_8475637847272940667_n-820x1024.jpg', title: 'Planet of the Apes' },
     { id: 4, foto: 'src/photos/fpMYRZpRpyDqXhvTPIz9O4ApXsqHGj3ssMxatQH5-810x1200.jpg', title: 'Oppenheimer' },
-    { id: 5, foto: 'src/photos/MV5BMDQ0NjgyN2YtNWViNS00YjA3LTkxNDktYzFkZTExZGMxZDkxXkEyXkFqcGdeQXVyODE5NzE3OTE@._V1_.jpg', title: 'Dune' },
+    { id: 5, foto: 'src/photos/MV5BMDQ0NjgyN2YtNWViNS00YjA3LTkxNDktYzFkZTExZGMxZDkxXkEyXkFqcGdeQXVyODE5NzE3OTE._V1_.jpg', title: 'Dune' },
     { id: 6, foto: 'src/photos/MV5BMGI3YzQyMzctMzM5Zi00ZmIxLTg2NjEtMmUzMjc1ZTUxODhhXkEyXkFqcGc@._V1_.jpg', title: 'The Novince' },
-    { id: 7, foto: 'src/photos/MV5BMmY1ODUzZGItNDllOS00MDBhLTg4NmUtYjU4YjUxMGNlYmMwXkEyXkFqcGdeQXVyODE5NzE3OTE@._V1_.jpg', title: 'Blue Beetle' },
-    { id: 8, foto: 'src/photos/MV5BOGE2NWUwMDItMjA4Yi00N2Y3LWJjMzEtMDJjZTMzZTdlZGE5XkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_.jpg', title: 'The Batman' },
-    { id: 9, foto: 'src/photos/MV5BZWNjZWUwNDgtYTM4ZC00Zjk0LTg3ZWItNGEyZmVkZTIxZDk0XkEyXkFqcGc@._V1_.jpg', title: 'Bad Boys' },
-    { id: 10, foto: 'src/photos/MV5BYzZkOGUwMzMtMTgyNS00YjFlLTg5NzYtZTE3Y2E5YTA5NWIyXkEyXkFqcGdeQXVyMjkwOTAyMDU@._V1_.jpg', title: 'Black Adam' },
+    { id: 7, foto: 'src/photos/MV5BMmY1ODUzZGItNDllOS00MDBhLTg4NmUtYjU4YjUxMGNlYmMwXkEyXkFqcGdeQXVyODE5NzE3OTE._V1_.jpg', title: 'Blue Beetle' },
+    { id: 8, foto: 'src/photos/MV5BOGE2NWUwMDItMjA4Yi00N2Y3LWJjMzEtMDJjZTMzZTdlZGE5XkEyXkFqcGdeQXVyODk4OTc3MTY._V1_.jpg', title: 'The Batman' },
+    { id: 9, foto: 'src/photos/MV5BZWNjZWUwNDgtYTM4ZC00Zjk0LTg3ZWItNGEyZmVkZTIxZDk0XkEyXkFqcGc._V1_.jpg', title: 'Bad Boys' },
+    { id: 10, foto: 'src/photos/MV5BYzZkOGUwMzMtMTgyNS00YjFlLTg5NzYtZTE3Y2E5YTA5NWIyXkEyXkFqcGdeQXVyMjkwOTAyMDU._V1_.jpg', title: 'Black Adam' },
   ];
 
-  //dit is een functie die er voor zorgt dat je de recensies kunt opslaan zelfs als je pagina refresht
+  // 5. Laad recensies en films uit localStorage wanneer de pagina geladen wordt
   useEffect(() => {
     const storedRecensies = JSON.parse(localStorage.getItem('recensies')) || [];
     setRecensies(storedRecensies);
+
+    // 5. Films cachen in localStorage
+    // 3. kortere functies
+    const storedFilms = JSON.parse(localStorage.getItem('films'));
+    if (storedFilms) {
+      setFilms(storedFilms);
+    } else {
+      localStorage.setItem('films', JSON.stringify(defaultFilms));
+      setFilms(defaultFilms);
+    }
   }, []);
 
-//   hier worden paar dingen gedaan: als je op een poster klikt wordt de naam opgenomen mogelijkheid om een recesie in text box te schrijven een strerren
-//booredeling geven en als laatste gaat de scherm naar het plekje waar dat review zijn met de beoordelingen zodat je handmatig niet hoeft te scrollen
   const handleFilmClick = (film) => {
     setSelectedFilm(film);
-    setRecensie(''); 
+    setRecensie('');
     setBeoordeling(0);
     reviewRef.current.scrollIntoView({ behavior: 'smooth' });
   };
-//sterren beoordling als je op strek aan clickt
+
   const handleStarClick = (index) => {
     setBeoordeling(index + 1);
   };
-// als je je review geschreven hebt submit je dan je resultaten en worden de informatie dat werden doorgegeven opgeslagen zelfs als je refresht
-//het neemt de naam van de film op  aantal sterren en jou gegeven tekst
+
   const handleSubmit = (e) => {
     e.preventDefault();
     const newRecensie = { film: selectedFilm.title, beoordeling, recensie };
 
     const updatedRecensies = [...recensies, newRecensie].slice(-3);
     setRecensies(updatedRecensies);
-    
     localStorage.setItem('recensies', JSON.stringify(updatedRecensies));
-// wanner je precies 3 reviews hebt gegevens wordt er een alert getoont dat je een kortings code kan gebruiken
+
     if (updatedRecensies.length === 2) {
-      alert('Bedankt voor jou reviews als een bedankje: 10% korting! Gebruik de code: KORTING10');
+      alert('Bedankt voor jou reviews als een bedankje: 10% korting! gebruik de code: SuperUser1010');
     }
-    
+
     setRecensie('');
     setBeoordeling(0);
   };
 
   return (
-    <div className="recensie-container">
-      <div className="film-posters">
+    <section className="recensie-container">
+      <article className="film-posters">
         {films.map((film) => (
           <div key={film.id} className="film-poster" onClick={() => handleFilmClick(film)}>
-            <img src={film.foto} alt={film.title} />
+            {/* 7.alt toegevoegt bij elke dyamiche foto zodat zoekmachine weet wat voor soort foto het is */}
+            {/* 8. Gebruik gemaakt van lazy loading waardoor alleen de nodige fotos gerenderd worden. */}
+            <img src={film.foto} alt={`Poster van ${film.title}`} loading="lazy" />
           </div>
         ))}
-      </div>
-      <div className="review-section" ref={reviewRef}>
+      </article>
+      <article className="review-section" ref={reviewRef}>
         {selectedFilm && (
           <>
-            <h2>Recensie voor: {selectedFilm.title}</h2>
+            <header>
+              <h2>Recensie voor: {selectedFilm.title}</h2>
+            </header>
             <div className="star-rating">
               {[...Array(5)].map((star, index) => (
-                //hier uit kan je selecteren uit de strerren er wordt ook visueel getoont op hoeveel strerren je clickte
                 <FaStar
                   key={index}
                   className={`star ${index < beoordeling ? 'filled' : ''}`}
@@ -107,7 +115,9 @@ const Recensie = () => {
           <h2>Jouw recensies:</h2>
           {recensies.map((item, index) => (
             <div key={index} className="recensie-item">
-              <h3>{item.film}</h3>
+              <header>
+                <h3>{item.film}</h3>
+              </header>
               <div className="star-rating">
                 {[...Array(5)].map((star, i) => (
                   <FaStar key={i} className={`star ${i < item.beoordeling ? 'filled' : ''}`} />
@@ -117,8 +127,8 @@ const Recensie = () => {
             </div>
           ))}
         </div>
-      </div>
-    </div>
+      </article>
+    </section>
   );
 };
 
