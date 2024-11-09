0658e946625c344fde1a46ecc75dbd4fa44b21d2
kacperfl <kacperfl29@gmail.com>
2024-11-09 19:06:59 +0100

    live server #5

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/movietime/',  // Voeg dit toe met de naam van je repo
   plugins: [react()],
 })
